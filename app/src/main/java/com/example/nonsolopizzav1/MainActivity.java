8a283a4240a5fcee5d78827d4bef4be2746886e5
89DoS <ildony89@yahoo.it>
2020-03-23 19:37:07 +0100

    NsP Test 1

---
+++ app/src/main/java/com/example/nonsolopizzav1/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.nonsolopizzav1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.internal.RegisterListenerMethod;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    FirebaseAuth fAuth;
+    EditText phoneNumber, codeEnter;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView state;
+    CountryCodePicker codePicker;
+    String verificationId;
+    PhoneAuthProvider.ForceResendingToken token;
+    Boolean verificationInProgress = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fAuth = FirebaseAuth.getInstance();
+        phoneNumber = findViewById(R.id.phone);
+        codeEnter = findViewById(R.id.codeEnter);
+        progressBar = findViewById(R.id.progressBar);
+        nextBtn = findViewById(R.id.nextBtn);
+        state = findViewById(R.id.state);
+        codePicker = findViewById(R.id.ccp);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!verificationInProgress) {
+                    if (!phoneNumber.getText().toString().isEmpty() && phoneNumber.getText().toString().length() == 10) {
+                        String phoneNum = "+" + codePicker.getSelectedCountryCode() + phoneNumber.getText().toString();
+                        Log.d(TAG, "onClick: Phone NO -> " + phoneNum);
+                        progressBar.setVisibility(View.VISIBLE);
+                        state.setText("Invio OTP..");
+                        state.setVisibility(View.VISIBLE);
+                        requestOPT(phoneNum);
+
+                    } else {
+                        phoneNumber.setError("Il numero non è valido");
+                    }
+                } else{
+                     String userOtp = codeEnter.getText().toString();
+                     if(!userOtp.isEmpty()&& userOtp.length() == 6){
+                         PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, userOtp);
+                         verifyAuth(credential);
+
+                     }else{
+                         codeEnter.setError("è richiesto un OTP valido");
+                     }
+                }
+            }
+        });
+
+    }
+
+    private void verifyAuth(PhoneAuthCredential credential) {
+        fAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Autenticazione avvenuta", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Autenticazione fallita", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void requestOPT(String phoneNum) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNum, 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                progressBar.setVisibility(View.GONE);
+                state.setVisibility(View.GONE);
+                codeEnter.setVisibility(View.VISIBLE);
+                verificationId = s;
+                token = forceResendingToken;
+                nextBtn.setText("Verifica");
+                verificationInProgress = true;
+
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                Toast.makeText(MainActivity.this, "impossibile creare un account" +e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
